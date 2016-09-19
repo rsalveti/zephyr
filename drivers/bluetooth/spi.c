f6526d6e6c030a44e6f8a682b4653b46d27d6b9a
Ricardo Salveti <ricardo.salveti@linaro.org>
2016-09-19 00:02:42 -0300

    First working version of the BT SPI driver
    Tested with the beacon example, several fixes and improvements still to
    be done.
    
    Change-Id: I74c562f751b026f5759fb04a133f0d994342ca2a
    Signed-off-by: Ricardo Salveti <ricardo.salveti@linaro.org>

--- drivers/bluetooth/spi.c
+++ drivers/bluetooth/spi.c
@@ -27,6 +27,7 @@
 #include <board.h>
 #include <init.h>
 #include <spi.h>
+#include <gpio.h>
 #include <misc/util.h>
 #include <misc/byteorder.h>
 #include <misc/stack.h>
@@ -38,13 +39,12 @@
 #include <bluetooth/hci.h>
 #include <bluetooth/driver.h>
 
-#include "util.h"
-
 #if !defined(CONFIG_BLUETOOTH_DEBUG_DRIVER)
 #undef BT_DBG
 #define BT_DBG(fmt, ...)
 #endif
 
+#if 0
 static BT_STACK_NOINIT(tx_stack, 256);
 static BT_STACK_NOINIT(rx_stack, 256);
 
@@ -249,41 +249,6 @@ static void spi_print_header(const uint8_t *hdr, const char *str)
 		       H5_HDR_PKT_TYPE(hdr), H5_HDR_LEN(hdr));
 	}
 }
-
-#if defined(CONFIG_BLUETOOTH_DEBUG_DRIVER)
-static void hexdump(const char *str, const uint8_t *packet, size_t length)
-{
-	int n = 0;
-
-	if (!length) {
-		printf("%s zero-length signal packet\n", str);
-		return;
-	}
-
-	while (length--) {
-		if (n % 16 == 0) {
-			printf("%s %08X ", str, n);
-		}
-
-		printf("%02X ", *packet++);
-
-		n++;
-		if (n % 8 == 0) {
-			if (n % 16 == 0) {
-				printf("\n");
-			} else {
-				printf(" ");
-			}
-		}
-	}
-
-	if (n % 16) {
-		printf("\n");
-	}
-}
-#else
-#define hexdump(str, packet, length)
-#endif
 
 static uint8_t spi_slip_byte(uint8_t byte)
 {
@@ -729,21 +694,270 @@ static void spi_init(void)
 	nano_delayed_work_init(&ack_work, ack_timeout);
 	nano_delayed_work_init(&retx_work, retx_timeout);
 }
+#endif
+
+#if defined(CONFIG_BLUETOOTH_DEBUG_DRIVER)
+static void hexdump(const char *str, const uint8_t *packet, size_t length)
+{
+	int n = 0;
+
+	if (!length) {
+		printf("%s zero-length signal packet\n", str);
+		return;
+	}
+
+	while (length--) {
+		if (n % 16 == 0) {
+			printf("%s %08X ", str, n);
+		}
+
+		printf("%02X ", *packet++);
+
+		n++;
+		if (n % 8 == 0) {
+			if (n % 16 == 0) {
+				printf("\n");
+			} else {
+				printf(" ");
+			}
+		}
+	}
+
+	if (n % 16) {
+		printf("\n");
+	}
+}
+#else
+#define hexdump(str, packet, length)
+#endif
+
+#define SPI_RECV_FIBER_STACK_SIZE 1024
+static char __stack spi_recv_fiber_stack[SPI_RECV_FIBER_STACK_SIZE];
+struct nano_sem nano_sem_req;
+
+static struct device *spi_dev;
+static struct device *gpio_dev;
+
+/* Frame format: 0 = Motorola, 1 = TI */
+#define SET_FRAME_FMT	0
+/* Hard-code size to 8 bits */
+#define SET_FRAME_SIZE	SPI_WORD(8)
+#define OP_MODE		SOC_MASTER_MODE
+#define SPI_SLAVE	0
+
+/* TODO: Make this generic */
+#if defined(CONFIG_SPI_STM32)
+#include <spi/spi_stm32.h>
+#define SPI_MAX_CLK_FREQ_250KHZ	128
+#define SOC_MASTER_MODE		SPI_STM32_MASTER_MODE
+#define SOC_SLAVE_MODE		SPI_STM32_SLAVE_MODE
+#define SPI_DRV_NAME		CONFIG_SPI_0_NAME
+/* TODO: Extract values from Kconfig */
+#define GPIO_DRV_NAME		"GPIOB"
+#define GPIO_REQ_DIR		GPIO_DIR_IN
+#define GPIO_REQ_PULL		GPIO_PUD_PULL_DOWN
+#define GPIO_REQ_PIN		1
+#define GPIO_EDGE		(GPIO_INT_EDGE | GPIO_INT_DOUBLE_EDGE)
+#endif
+
+/* 2 bytes are used for the header size */
+#define HEADER_SIZE	2
+
+static struct gpio_callback gpio_cb;
+static int prev_slave_req = 0;
+
+struct spi_config spi_conf = {
+	.config = (SET_FRAME_SIZE | OP_MODE | SPI_STM32_SLAVE_HW_SS_OUTPUT),
+	.max_sys_freq = SPI_MAX_CLK_FREQ_250KHZ,
+};
+
+static void _spi_show(struct spi_config *spi_conf)
+{
+	BT_DBG("SPI Configuration: %x", spi_conf->config);
+	BT_DBG("\tbits per word: %u",
+			SPI_WORD_SIZE_GET(spi_conf->config));
+	BT_DBG("\tMode: %u", SPI_MODE(spi_conf->config));
+	BT_DBG("\tMax speed Hz: 0x%X", spi_conf->max_sys_freq);
+#if defined(CONFIG_SPI_STM32)
+	BT_DBG("\tOperating mode: %s",
+			SPI_STM32_OP_MODE_GET(spi_conf->config)? "slave": "master");
+#endif
+}
+
+void gpio_slave_req(struct device *gpio, struct gpio_callback *cb,
+		    uint32_t pins)
+{
+	int slave_req;
+
+	gpio_pin_read(gpio, GPIO_REQ_PIN, &slave_req);
+	/*
+	* FIXME: multiple interrupts are generated during spi transceive,
+	* so for now just store and maintain the previous value
+	*/
+	if (slave_req != prev_slave_req) {
+		prev_slave_req = slave_req;
+		if (prev_slave_req == 1) {
+			nano_isr_sem_give(&nano_sem_req);
+		}
+	}
+}
+
+static void spi_recv_fiber(void)
+{
+	BT_DBG("");
+
+	uint8_t spi_tx_buf[2];
+	uint8_t spi_rx_buf[255];
+	uint16_t spi_buf_len;
+	uint8_t bt_buf_type;
+	struct net_buf *buf;
+	int ret;
+
+	while (1) {
+		nano_fiber_sem_take(&nano_sem_req, TICKS_UNLIMITED);
+
+		BT_DBG("SPI slave request to send buf");
+
+		/* Send empty header, announce we are ready to receive data */
+		BT_DBG("header - empty, announce ready to receive");
+		memset(spi_tx_buf, 0, sizeof(spi_tx_buf));
+		ret = spi_transceive(spi_dev, spi_tx_buf, sizeof(spi_tx_buf),
+						spi_rx_buf, 2);
+		if (ret < 0) {
+			BT_ERR("Error in spi_transceive: %d", ret);
+			continue;
+		}
+
+		/* TODO: change to use the /RDY pin */
+		fiber_sleep(MSEC(150));
+
+		/* First read is the header, which contains the buffer size */
+		memset(&spi_rx_buf, 0, sizeof(spi_rx_buf));
+		ret = spi_transceive(spi_dev, spi_tx_buf, sizeof(spi_tx_buf),
+						spi_rx_buf, HEADER_SIZE);
+		if (ret < 0) {
+			BT_ERR("1Failed to read from SPI, err %d", ret);
+			continue;
+		}
+
+		/* TODO: handle different buf sizes */
+		memcpy(&spi_buf_len, spi_rx_buf, HEADER_SIZE);
+		BT_DBG("Header: buf size %d", spi_buf_len);
+
+		/* TODO: change to use the /RDY pin */
+		fiber_sleep(MSEC(150));
+
+		memset(&spi_rx_buf, 0, sizeof(spi_rx_buf));
+		ret = spi_transceive(spi_dev, spi_tx_buf, sizeof(spi_tx_buf),
+				spi_rx_buf, spi_buf_len);
+		if (ret < 0) {
+			BT_ERR("Failed to read from SPI, err %d", ret);
+			continue;
+		}
+
+		bt_buf_type = spi_rx_buf[0];
+		switch (bt_buf_type) {
+		case BT_BUF_EVT:
+			BT_DBG("BT rx buf type EVT");
+			buf = bt_buf_get_evt(bt_buf_type);
+			if (!buf) {
+				BT_ERR("No available event buffers!");
+				continue;
+			}
+			break;
+		case BT_BUF_ACL_IN:
+			BT_DBG("BT rx buf type ACL_IN");
+			buf = bt_buf_get_acl();
+			if (!buf) {
+				BT_ERR("No available ACL buffers!");
+				continue;
+			}
+			break;
+		default:
+			BT_ERR("UNKNOWN BT BUF TYPE %d", bt_buf_type);
+			continue;
+		}
+
+		memcpy(net_buf_add(buf, spi_buf_len - 1), spi_rx_buf + 1,
+						spi_buf_len - 1);
+		hexdump("=>", buf->data, buf->len);
+		bt_recv(buf);
+		buf = NULL;
+	}
+}
+
+static int spi_send(struct net_buf *buf)
+{
+	uint8_t spi_tx_buf[255];
+	uint8_t spi_rx_buf[1];
+	uint16_t spi_buf_len;
+	int ret;
+
+	BT_DBG("buf %p type %u len %u", buf, bt_buf_get_type(buf), buf->len);
+
+	if (buf->len) {
+		hexdump("<= ", buf->data, buf->len);
+	}
+
+	/* First send the header, which contains the tx buffer size */
+	memset(spi_tx_buf, 0, sizeof(spi_tx_buf));
+	spi_buf_len = buf->len + 1; /* extra byte for buffer type */
+	memcpy(spi_tx_buf, &spi_buf_len, sizeof(spi_buf_len));
+	BT_DBG("header - spi buf len: %d", spi_buf_len);
+	ret = spi_transceive(spi_dev, spi_tx_buf, 2, spi_rx_buf, 1);
+	if (ret < 0) {
+		BT_ERR("Error in spi_transceive: %d", ret);
+		return -EIO;
+	}
+
+	/* FIXME: can't go too fast, otherwise nRF51 will fail to read data.
+	 * TODO: change to use the /RDY pin.
+	 */
+	fiber_sleep(MSEC(150));
+
+	/* Now the data, until everything is transmited */
+	/* FIXME: allow buf > spi_tx_buf by transmitting multiple times */
+	memset(spi_tx_buf, 0, sizeof(spi_tx_buf));
+	spi_tx_buf[0] = (uint8_t) bt_buf_get_type(buf);
+	memcpy(spi_tx_buf + 1, buf->data, buf->len);
+	BT_DBG("sending command buffer, len: %d", buf->len + 1);
+	ret = spi_transceive(spi_dev, spi_tx_buf, buf->len + 1, spi_rx_buf, 1);
+	if (ret < 0) {
+		BT_ERR("Error in spi_transceive: %d", ret);
+		return -EIO;
+	}
+
+	net_buf_unref(buf);
+
+	return 0;
+}
 
 static int spi_open(void)
 {
 	BT_DBG("");
 
-	uart_irq_rx_disable(spi_dev);
-	uart_irq_tx_disable(spi_dev);
+	spi_configure(spi_dev, &spi_conf);
+	spi_slave_select(spi_dev, SPI_SLAVE);
+	_spi_show(&spi_conf);
 
-	bt_uart_drain(spi_dev);
+	/* GPIOs, /REQ and /RDY */
+	gpio_dev = device_get_binding(GPIO_DRV_NAME);
+	if (gpio_dev == NULL) {
+		BT_ERR("Failed to initialize GPIO driver %s",
+						GPIO_DRV_NAME);
+		return -EIO;
+	}
+	/* TODO: Add /RDY */
+	gpio_pin_configure(gpio_dev, GPIO_REQ_PIN, GPIO_REQ_DIR |
+						GPIO_INT | GPIO_EDGE);
+	gpio_init_callback(&gpio_cb, gpio_slave_req, BIT(GPIO_REQ_PIN));
+	gpio_add_callback(gpio_dev, &gpio_cb);
+	gpio_pin_enable_callback(gpio_dev, GPIO_REQ_PIN);
 
-	uart_irq_callback_set(spi_dev, bt_spi_isr);
+	nano_sem_init(&nano_sem_req);
 
-	spi_init();
-
-	uart_irq_rx_enable(spi_dev);
+	fiber_start(spi_recv_fiber_stack, sizeof(spi_recv_fiber_stack),
+			(nano_fiber_entry_t) spi_recv_fiber, 0, 0, 7, 0);
 
 	return 0;
 }
@@ -752,7 +966,7 @@ static struct bt_driver drv = {
 	.name		= "SPI_Bus",
 	.bus		= BT_DRIVER_BUS_SPI,
 	.open		= spi_open,
-	.send		= spi_queue,
+	.send		= spi_send,
 };
 
 static int _bt_spi_init(struct device *unused)
@@ -760,7 +974,6 @@ static int _bt_spi_init(struct device *unused)
 	ARG_UNUSED(unused);
 
 	spi_dev = device_get_binding(CONFIG_BLUETOOTH_SPI_ON_DEV_NAME);
-
 	if (spi_dev == NULL) {
 		return -EINVAL;
 	}
--- samples/bluetooth/bt_spi_tunnel/src/bt_spi_tunnel.c
+++ samples/bluetooth/bt_spi_tunnel/src/bt_spi_tunnel.c
@@ -18,15 +18,15 @@
  */
 
 #include <nanokernel.h>
+#include <stdio.h>
 
 #define SYS_LOG_LEVEL SYS_LOG_SPI_LEVEL
 #include <misc/sys_log.h>
-#include <misc/printk.h>
-#include <misc/util.h>
 
 #include <errno.h>
 
 #include <device.h>
+#include <gpio.h>
 
 #include <net/buf.h>
 #include <bluetooth/buf.h>
@@ -62,6 +62,13 @@
  */
 
 
+#define SPI_RX_FIBER_STACK_SIZE 1024
+static char __stack bt_spi_rx_fiber_stack[SPI_RX_FIBER_STACK_SIZE];
+
+static struct device *spi_dev;
+static struct device *gpio_dev;
+
+#if 0
 #define BTSPI_BUFFER_SIZE 64
 
 /* Max Bluetooth command data size */
@@ -70,24 +77,25 @@
 /* Misc. macros */
 #define LOW_BYTE(x)	((x) & 0xFF)
 #define HIGH_BYTE(x)	((x) >> 8)
+#endif
 
 static struct nano_fifo rx_queue;
 
 /* HCI command buffers */
-#define CMD_BUF_SIZE (CONFIG_BLUETOOTH_HCI_SEND_RESERVE +	  \
-			sizeof(struct bt_hci_cmd_hdr) +	  \
-			CONFIG_BLUETOOTH_MAX_CMD_LEN)
+#define CMD_BUF_SIZE (CONFIG_BLUETOOTH_HCI_SEND_RESERVE + \
+		      sizeof(struct bt_hci_cmd_hdr) + \
+		      CONFIG_BLUETOOTH_MAX_CMD_LEN)
 
 static struct nano_fifo avail_tx;
 static NET_BUF_POOL(tx_pool, CONFIG_BLUETOOTH_HCI_CMD_COUNT, CMD_BUF_SIZE,
-		&avail_tx, NULL, sizeof(uint8_t));
+		    &avail_tx, NULL, sizeof(uint8_t));
 
 #define BT_L2CAP_MTU 64
 /** Data size needed for ACL buffers */
-#define BT_BUF_ACL_SIZE (CONFIG_BLUETOOTH_HCI_RECV_RESERVE +		\
-				sizeof(struct bt_hci_acl_hdr) +	\
-				4 /* L2CAP header size */ +		\
-				BT_L2CAP_MTU)
+#define BT_BUF_ACL_SIZE (CONFIG_BLUETOOTH_HCI_RECV_RESERVE + \
+			 sizeof(struct bt_hci_acl_hdr) + \
+			 4 /* L2CAP header size */ + \
+			 BT_L2CAP_MTU)
 
 static struct nano_fifo avail_acl_tx;
 static NET_BUF_POOL(acl_tx_pool, 2, BT_BUF_ACL_SIZE, &avail_acl_tx, NULL,
@@ -96,18 +104,26 @@ static NET_BUF_POOL(acl_tx_pool, 2, BT_BUF_ACL_SIZE, &avail_acl_tx, NULL,
 /* FIXME: This make it nRF5 specific, generalise it later */
 #define OP_MODE SPI_NRF5_OP_MODE_SLAVE
 
-enum spi_status_t {
-	SPI_UNKNOWN = -1,
-	SPI_SEND = 1,
+/* TODO: Move to a proper place */
+/* Slave uses RDY and REQ pins to coordinate the messages with master */
+#define GPIO_DRV_NAME	"GPIO_0"
+#define GPIO_RDY_DIR	GPIO_DIR_OUT
+#define GPIO_RDY_PULL	GPIO_PUD_PULL_DOWN
+#define GPIO_RDY_PIN	29
+#define GPIO_REQ_DIR	GPIO_DIR_OUT
+#define GPIO_REQ_PULL	GPIO_PUD_PULL_DOWN
+#define GPIO_REQ_PIN	28 /* Use 22 on PCA10028, avoid conflict with carbon */
+
+/* 2 bytes are used for the header size */
+#define HEADER_SIZE	2
+
+static struct spi_config btspi_config = {
+	.config = (SPI_WORD(8) | OP_MODE),
+	.max_sys_freq = 128,
 };
 
-/* Tunnel state */
-struct spi_tunnel_data_t {
-	struct device *spi_dev;
-	enum spi_status_t spi_status;
-	uint32_t tx_bytes;
-	uint32_t rx_bytes;
-};
+struct nano_sem nano_sem_fiber;
+struct nano_sem nano_sem_task;
 
 /* TODO: move to standard utils */
 static void hexdump(const char *str, const uint8_t *packet, size_t length)
@@ -115,94 +131,207 @@ static void hexdump(const char *str, const uint8_t *packet, size_t length)
 	int n = 0;
 
 	if (!length) {
-		printk("%s zero-length signal packet\n", str);
+		printf("%s zero-length signal packet\n", str);
 		return;
 	}
 
 	while (length--) {
 		if (n % 16 == 0) {
-			printk("%s %08X ", str, n);
+			printf("%s %08X ", str, n);
 		}
 
-		printk("%02X ", *packet++);
+		printf("%02X ", *packet++);
 
 		n++;
 		if (n % 8 == 0) {
 			if (n % 16 == 0) {
-				printk("\n");
+				printf("\n");
 			} else {
-				printk(" ");
+				printf(" ");
 			}
 		}
 	}
 
 	if (n % 16) {
-		printk("\n");
+		printf("\n");
 	}
 }
 
-static int try_write(struct device *dev, struct net_buf *buf)
+static int bt_spi_tx(uint8_t bt_buf_type, struct net_buf *buf)
 {
-	while (1) {
-		int ret = spi_write(dev, buf->data, buf->len);
+	uint8_t spi_tx_buf[255];
+	uint8_t spi_rx_buf[1] = { 0 };
+	uint32_t spi_buf_len;
+	int ret;
 
-		switch (ret) {
-		case -EAGAIN:
-			break;
-		/* TODO: Handle other error codes */
-		default:
-			return ret;
-		}
+	/* To send data we first must notify the master side with /REQ */
+	SYS_LOG_DBG("setting /REQ to 1");
+	gpio_pin_write(gpio_dev, GPIO_REQ_PIN, 1);
+
+	/* Wait until rx fiber says we're good to go */
+	SYS_LOG_DBG("sem take task, wait for rx fiber");
+	nano_task_sem_take(&nano_sem_task, TICKS_UNLIMITED);
+	gpio_pin_write(gpio_dev, GPIO_REQ_PIN, 0);
+
+	/* Send the header, containing the buf size */
+	memset(spi_tx_buf, 0, sizeof(spi_tx_buf));
+	spi_buf_len = buf->len + 1; /* extra byte for buffer type */
+	memcpy(spi_tx_buf, &spi_buf_len, sizeof(spi_buf_len));
+	SYS_LOG_DBG("header - spi buf len: %d", spi_buf_len);
+	ret = spi_transceive(spi_dev, spi_tx_buf, 2, spi_rx_buf, 1);
+	if (ret < 0) {
+		SYS_LOG_ERR("Error in spi_transceive: %d", ret);
+		nano_task_sem_give(&nano_sem_fiber);
+		return -EIO;
 	}
+
+	/* Then send the data */
+	/* TODO: transmit multiple times when buf > tx_buf */
+	/* TODO: should we send buf type together with header, for zero copy? */
+	memset(spi_tx_buf, 0, sizeof(spi_tx_buf));
+	spi_tx_buf[0] = bt_buf_type;
+	memcpy(spi_tx_buf + 1, buf->data, buf->len);
+	SYS_LOG_DBG("sending command buffer, len: %d", buf->len + 1);
+	ret = spi_transceive(spi_dev, spi_tx_buf, buf->len + 1, spi_rx_buf, 1);
+	if (ret < 0) {
+		SYS_LOG_ERR("Error in spi_transceive: %d", ret);
+		nano_task_sem_give(&nano_sem_fiber);
+		return -EIO;
+	}
+
+	SYS_LOG_DBG("sem give fiber");
+	nano_task_sem_give(&nano_sem_fiber);
+
+	return 0;
 }
 
-static struct spi_config btspi_config = {
-	.config = (SPI_WORD(8) | OP_MODE),
-	.max_sys_freq = 128,
-};
+/* Fiber responsible for receiving data from master */
+static void bt_spi_rx_fiber(void)
+{
+	uint8_t spi_rx_buf[255];
+	uint8_t spi_tx_buf[1] = { 0 };
+	uint32_t header;
+	uint8_t bt_buf_type;
+	struct net_buf *buf;
+	int ret;
 
-static struct spi_tunnel_data_t btspi_data = {
-	.spi_status = SPI_UNKNOWN,
-};
+	SYS_LOG_DBG("Starting bt_spi_rx_fiber");
+
+	while (1) {
+		/* First read is the header, which contains the buffer size */
+		memset(&spi_rx_buf, 0, sizeof(spi_rx_buf));
+		ret = spi_transceive(spi_dev, spi_tx_buf, sizeof(spi_tx_buf),
+					spi_rx_buf, HEADER_SIZE);
+		if (ret != 0) {
+			SYS_LOG_ERR("Failed to read from SPI, err %d", ret);
+			continue;
+		}
+		/* Check buffer content:
+		 * If buffer contains only 0, master ready to accept data
+		 * If buffer > 0, read buffer and add to the queue
+		 */
+		memcpy(&header, spi_rx_buf, HEADER_SIZE);
+		SYS_LOG_DBG("Header: buf size %d", header);
+		if (header == 0) {
+			/* Let the tx task to do its job and wait */
+			SYS_LOG_DBG("sem give task");
+			nano_fiber_sem_give(&nano_sem_task);
+			SYS_LOG_DBG("sem take fiber, wait for tx task");
+			nano_fiber_sem_take(&nano_sem_fiber, TICKS_UNLIMITED);
+			continue;
+		}
+		memset(&spi_rx_buf, 0, sizeof(spi_rx_buf));
+		ret = spi_transceive(spi_dev, spi_tx_buf, sizeof(spi_tx_buf),
+					spi_rx_buf, header);
+		if (ret != 0) {
+			SYS_LOG_ERR("Failed to read from SPI, err %d", ret);
+			continue;
+		}
+		bt_buf_type = spi_rx_buf[0];
+		switch (bt_buf_type) {
+		case BT_BUF_CMD:
+			SYS_LOG_DBG("BT rx buf type BUF_CMD");
+			buf = net_buf_get(&avail_tx, 0);
+			if (!buf) {
+				SYS_LOG_ERR("Cannot get free tx buffer");
+				continue;
+			}
+			break;
+		case BT_BUF_ACL_OUT:
+			SYS_LOG_DBG("BT rx buf type ACL_OUT");
+			buf = net_buf_get(&avail_acl_tx, 0);
+			if (!buf) {
+				SYS_LOG_ERR("Cannot get free acl tx buffer");
+				continue;
+			}
+			break;
+		default:
+			SYS_LOG_ERR("Unknown bluetooth buffer type");
+			continue;
+		}
+
+		bt_buf_set_type(buf, bt_buf_type);
+		memcpy(net_buf_add(buf, header - 1), spi_rx_buf + 1,
+							header - 1);
+		hexdump(">", buf->data, buf->len);
+		bt_send(buf);
+	}
+}
 
 void main(void)
 {
-	struct device *spi_dev;
 	int ret;
 
-	SYS_LOG_DBG("Start");
+	SYS_LOG_DBG("\n\nStart\n");
 
 	spi_dev = device_get_binding(CONFIG_BLUETOOTH_SPI_ON_DEV_NAME);
 	if (!spi_dev) {
-		SYS_LOG_ERR("Cannot find device %s", CONFIG_BLUETOOTH_SPI_ON_DEV_NAME);
+		SYS_LOG_ERR("Cannot find device %s",
+				CONFIG_BLUETOOTH_SPI_ON_DEV_NAME);
+		return;
 	}
-	btspi_data.spi_dev = spi_dev;
 
 	/* Initialize the SPI driver with the right configuration */
 	ret = spi_configure(spi_dev, &btspi_config);
 	if (ret < 0) {
-		SYS_LOG_ERR("Failed to configure %s", CONFIG_BLUETOOTH_SPI_ON_DEV_NAME);
+		SYS_LOG_ERR("Failed to configure %s",
+				CONFIG_BLUETOOTH_SPI_ON_DEV_NAME);
 		return;
 	}
+
+	/* Initialize the /REQ and /RDY GPIO pins */
+	gpio_dev = device_get_binding(GPIO_DRV_NAME);
+	gpio_pin_configure(gpio_dev, GPIO_RDY_PIN,
+				GPIO_RDY_DIR | GPIO_RDY_PULL);
+	gpio_pin_configure(gpio_dev, GPIO_REQ_PIN,
+				GPIO_REQ_DIR | GPIO_REQ_PULL);
 
 	/* Initialize the buffer pools */
 	net_buf_pool_init(tx_pool);
 	net_buf_pool_init(acl_tx_pool);
 	nano_fifo_init(&rx_queue);
 
+	nano_sem_init(&nano_sem_fiber);
+	nano_sem_init(&nano_sem_task);
+
+	/* Receiver fiber */
+	fiber_start(bt_spi_rx_fiber_stack, sizeof(bt_spi_rx_fiber_stack),
+			(nano_fiber_entry_t) bt_spi_rx_fiber, 0, 0, 7, 0);
+
 	bt_enable_raw(&rx_queue);
 
 	while (1) {
 		struct net_buf *buf;
+		uint8_t bt_buf_type;
 
+		/* With TICKS_UNLIMITED we always get a valid buffer */
 		buf = net_buf_get_timeout(&rx_queue, 0, TICKS_UNLIMITED);
-    if (buf) {
-            hexdump("<", buf->data, buf->len);
-            ret = try_write(spi_dev, buf);
-            net_buf_unref(buf);
-    } else {
-            printk("No buf\n");
-            fiber_yield();
-    }
+		bt_buf_type = bt_buf_get_type(buf);
+
+		hexdump("<", buf->data, buf->len);
+
+		bt_spi_tx(bt_buf_type, buf);
+
+		net_buf_unref(buf);
 	}
 }
