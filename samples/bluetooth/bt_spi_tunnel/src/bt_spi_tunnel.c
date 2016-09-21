56eafc9bec6de2ed18275cc08ee47fcabf611201
Ricardo Salveti <ricardo.salveti@linaro.org>
2016-09-21 17:39:47 -0300

    samples/bt_spi_tunnel: move architecture layout to the README file
    Change-Id: Ie812c695266b63b986822d5320a488bf5016297f
    Signed-off-by: Ricardo Salveti <ricardo.salveti@linaro.org>

--- samples/bluetooth/bt_spi_tunnel/src/bt_spi_tunnel.c
+++ samples/bluetooth/bt_spi_tunnel/src/bt_spi_tunnel.c
@@ -39,31 +39,6 @@
 #include <spi/spi_nrf5.h>
 #endif
 
-/*
- *  \|/
- *   |
- *   |           [   Zephyr   ]           \       \       [   Zephyr   ]
- * [Radio] <---> [ controller ] <--XXXX---/ Break /--XX-> [  BLE HCI   ] <---> [Application]
- *               [   driver   ]           \       \       [   driver   ]
- *                     ^                                        ^
- *                     |                                        |
- *                     |                                        |
- *                     v                                        v
- *               [ Raw HCI driver ]                    [ Zephyr HCI stack ]
- *                     ^                                        ^
- *                     |                                        |
- *                     |                                        |
- *                     v                                        v
- *               [ This program ]                    [ BLE SPI Transport driver ]
- *                     |                                        |
- *                     |-------------- [ SPI Tunnel ] ----------|
- *                        (slave)                      (master)
- *
- *    [Connectivity processor]                       [ Application Processor ]
- *    [      (e.g. nRF51)    ]                       [      (e.g. STM43F4)   ]
- */
-
-
 #define SPI_RX_FIBER_STACK_SIZE 1024
 #define SPI_TX_FIBER_STACK_SIZE 1024
 static char __stack bt_spi_rx_fiber_stack[SPI_RX_FIBER_STACK_SIZE];
